6421074642d5a571e06008c84029a50215526dfe
aquapi <aquaplmc@gmail.com>
2024-09-14 23:17:38 +0700

    Fix compiler bug

--- src/tree/compiler.ts
+++ src/tree/compiler.ts
@@ -1,5 +1,5 @@
 import type { Node } from './node';
-import type { CompilerState } from '@mapl/compiler';
+import { getContent, getExternalKeys, type CompilerState } from '@mapl/compiler';
 
 type ParametersExcludeState<T> = T extends (arg0: any, arg1: any, ...rest: infer R) => any ? R : never;
 
@@ -33,11 +33,16 @@ export function compileNode(
 
   // Same optimization as in the matcher
   startIndexValue++;
-  if (partLen !== 1) for (let i = 1; i < partLen; i++, startIndexValue++) builder.push(`if(${PATHNAME}.charCodeAt(${startIndexPrefix}${startIndexValue})===${part.charCodeAt(i)})`);
+  if (partLen !== 1) {
+    builder.push(`if(${PATHNAME_LEN}>${startIndexPrefix}${startIndexValue + partLen - 2})`);
+    for (let i = 1; i < partLen; i++, startIndexValue++) builder.push(`if(${PATHNAME}.charCodeAt(${startIndexPrefix}${startIndexValue})===${part.charCodeAt(i)})`);
+    builder.push('{');
+  }
 
   if (node[1] !== null) {
     builder.push(`if(${PATHNAME_LEN}===${startIndexPrefix}${startIndexValue}){`);
     state.compileItem(node[1], state, hasParam, hasMultipleParams, startIndexValue, startIndexPrefix);
+    builder.push('}');
   }
 
   if (node[2] !== null) {
@@ -56,11 +61,11 @@ export function compileNode(
       builder.push(`switch(${PATHNAME}.charCodeAt(${startIndexPrefix}${startIndexValue})){`);
 
       for (let i = 0, l = childrenKeys.length; i < l; i++) {
-        builder.push(`case ${childrenKeys[i]}:{`);
+        builder.push(`case ${childrenKeys[i]}:`);
         // @ts-expect-error Key exists
         // eslint-disable-next-line
         compileNode(children[childrenKeys[i]], state, hasParam, hasMultipleParams, startIndexValue, startIndexPrefix);
-        builder.push('break;}');
+        builder.push('break;');
       }
 
       builder.push('}');
@@ -69,15 +74,18 @@ export function compileNode(
 
   if (node[3] !== null) {
     const params = node[3];
+    const hasStore = params[1] !== null;
+    const hasChild = params[0] !== null;
+
+    // Whether to wrap the parameter check in a scope
+    const requireAllocation = hasParam ? hasMultipleParams : hasChild || !hasStore;
+    if (requireAllocation) builder.push('{');
 
     // Declare a variable to save previous param index
     if (hasParam) builder.push(`${hasMultipleParams ? '' : 'let '}${PREV_PARAM_INDEX}=${startIndexPrefix}${startIndexValue};`);
 
     const currentIndex = hasParam ? PREV_PARAM_INDEX : `${startIndexPrefix}${startIndexValue}`;
-    const slashIndex = `${PATHNAME}.indexOf('/',${currentIndex});`;
-
-    const hasStore = params[1] !== null;
-    const hasChild = params[0] !== null;
+    const slashIndex = `${PATHNAME}.indexOf('/',${currentIndex})`;
 
     // Need to save the current parameter index if the parameter node is not a leaf node
     if (hasChild || !hasStore)
@@ -91,9 +99,12 @@ export function compileNode(
 
     if (hasChild) {
       builder.push(`if(${hasStore ? '' : `${CURRENT_PARAM_INDEX}!==-1&&`}${CURRENT_PARAM_INDEX}!==${currentIndex}){${PARAMS}.push(${PATHNAME}.substring(${currentIndex},${CURRENT_PARAM_INDEX}));`);
-      compileNode(params[0]!, state, true, hasParam, 1, `${CURRENT_PARAM_INDEX}+`);
-      builder.push('}');
+      compileNode(params[0]!, state, true, hasParam, 0, `${CURRENT_PARAM_INDEX}+`);
+      builder.push(`${PARAMS}.pop();}`);
     }
+
+    // Close the scope
+    if (requireAllocation) builder.push('}');
   }
 
   if (node[4] !== null) {
@@ -110,4 +121,24 @@ export function compileNode(
 
   if (partLen !== 1)
     builder.push('}');
+}
+
+// eslint-disable-next-line
+const compileItem: RouterCompilerState<any>['compileItem'] = (item, state) => {
+  const itemId = state.externalValues.length;
+  state.externalValues.push(item);
+  state.contentBuilder.push(`return f${itemId};`);
+};
+
+export function compileMatcher(root: Node): (path: string, params: string[]) => any {
+  const state: RouterCompilerState<any> = {
+    contentBuilder: [],
+    declarationBuilders: [],
+    localVarCount: 0,
+    externalValues: [],
+    compileItem
+  };
+  compileNode(root, state, false, false, 0, '');
+  // eslint-disable-next-line
+  return Function(...getExternalKeys(state), `return (${PATHNAME},${PARAMS})=>{const ${PATHNAME_LEN}=${PATHNAME}.length;${getContent(state)}return null;}`)(...state.externalValues);
 }
--- tests/match.test.ts
+++ tests/match.test.ts
@@ -1,22 +1,51 @@
 import { describe, test, expect } from 'bun:test';
 
 import { matchItem } from '@mapl/router/tree/matcher';
+import { PARAMS, PATHNAME, PATHNAME_LEN, compileNode, type RouterCompilerState } from '@mapl/router/tree/compiler';
 import { createNode, insertItem } from '@mapl/router/tree/node';
+import { getExternalKeys, getContent } from '@mapl/compiler';
+
 import { samplePaths, samplePathsLen } from './datasets/paths';
 
-function toPath(pattern: string) {
-  return pattern.endsWith('**') ? pattern.substring(0, pattern.length - 2) + '1/2/3' : pattern;
-}
+const resultPaths = samplePaths.map((pattern) => pattern.endsWith('**') ? pattern.substring(0, pattern.length - 2) + '1/2/3' : pattern);
 
 const root = createNode('/');
 for (let i = 0; i < samplePathsLen; i++)
   insertItem(root, samplePaths[i], i);
 
-console.log(root)
-
 describe('Should match correctly', () => {
   for (let i = 0; i < samplePathsLen; i++)
     test(`${samplePaths[i]}: ${i}`, () => {
-      expect(matchItem(root, toPath(samplePaths[i]), [], 0)).toBe(i);
+      expect(matchItem(root, resultPaths[i], [], 0)).toBe(i);
+    });
+});
+
+describe('Compile matcher correctly', () => {
+  const state: RouterCompilerState<any> = {
+    contentBuilder: [],
+    declarationBuilders: [],
+    localVarCount: 0,
+    externalValues: [],
+
+    compileItem: (item, state) => {
+      const itemId = state.externalValues.length;
+      state.externalValues.push(item);
+      state.contentBuilder.push(`return f${itemId};`);
+    }
+  };
+
+  compileNode(root, state, false, false, 0, '');
+
+  // eslint-disable-next-line
+  const match = Function(
+    ...getExternalKeys(state),
+    `return (${PATHNAME},${PARAMS})=>{const ${PATHNAME_LEN}=${PATHNAME}.length;${getContent(state)}return null;}`
+  )(...state.externalValues);
+
+  console.log(match.toString());
+
+  for (let i = 0; i < samplePathsLen; i++)
+    test(`${samplePaths[i]}: ${i}`, () => {
+      expect(match(resultPaths[i], [])).toBe(i);
     });
 });
