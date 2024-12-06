e892204e7ed48c7362acf7e57cba122d64293d4e
aquapi <aquaplmc@gmail.com>
2024-12-07 06:56:26 +0700

    Shorter identifiers

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 // Compiler constants
-export const REQ = '__req';
-export const PATH = `${REQ}_p`;
-export const PATH_LEN = `${REQ}_pl`;
-export const PARAMS = `${REQ}_ps`;
-export const PREV_PARAM_IDX = `${REQ}_ppi`;
-export const CURRENT_PARAM_IDX = `${REQ}_cpi`;
+export const REQ = 'r';
+export const PATH = 'p';
+export const PATH_LEN = 'l';
+export const PARAMS = 'q';
+export const PREV_PARAM_IDX = 'i';
+export const CURRENT_PARAM_IDX = 'j';
