3ec5ea736f2431b03933cae6e39c3cedecc9e764
aquapi <aquaplmc@gmail.com>
2024-12-26 00:08:10 +0700

    Update

--- tests/utils/fastCompileRouter.ts
+++ tests/utils/fastCompileRouter.ts
@@ -1,5 +1,5 @@
 import { PATH } from '@mapl/router/constants';
-import buildRouter from '@mapl/router/fast-compile';
+import buildRouter, { injectMatcher } from '@mapl/router/fast-compile';
 
 import type { Router } from '@mapl/router/index';
 
@@ -8,7 +8,7 @@ export default (root: Router): (path: string) => any => {
   const decls: string[] = [];
   const deps: string[] = [];
 
-  const body = buildRouter(root, decls, deps, '');
+  const body = buildRouter(root, decls, deps, injectMatcher(deps), '');
   const result = `${decls.map((str, idx) => 'var d' + (idx + 1) + '=' + str + ';').join('')}return (${PATH})=>{${body}return null;}`;
 
   // eslint-disable-next-line
