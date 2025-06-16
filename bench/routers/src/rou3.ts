25b092dd5fe30323e549fe88034dc10572a3a78b
aquapi <aquaplmc@gmail.com>
2025-06-16 23:59:55 +0700

    Only enable baseline JIT for startup

--- bench/routers/src/rou3.ts
+++ bench/routers/src/rou3.ts
@@ -3,25 +3,28 @@ import { compileRouter } from "rou3/compiler";
 
 import type { Subject } from '../cases.js';
 
+const basicAPI = () => {
+  type Handler = (params: Record<string, string>) => string;
+  const router = createRouter<Handler>();
+
+  addRoute(router, 'GET', '/user', () => "0");
+  addRoute(router, 'GET', '/user/comments', () => "1");
+  addRoute(router, 'GET', '/user/avatar', () => "2");
+  addRoute(router, 'GET', '/user/lookup/username/:a', (params) => "3: " + params.a);
+  addRoute(router, 'GET', '/user/lookup/email/:a', (params) => "4: " + params.a);
+  addRoute(router, 'GET', '/event/:a', (params) => '5: ' + params.a);
+  addRoute(router, 'GET', '/event/:a/comments', (params) => "6: " + params.a);
+  addRoute(router, 'GET', '/map/:a/events', (params) => '7: ' + params.a);
+  addRoute(router, 'GET', '/status', () => '8');
+  addRoute(router, 'GET', '/very/deeply/nested/route/hello/there', () => '9');
+
+  addRoute(router, 'POST', '/event/:a/comment', (params) => '10: ' + params.a);
+  return router;
+}
+
 export const jit = {
   'basic-api': () => {
-    type Handler = (params: Record<string, string>) => string;
-    const router = createRouter<Handler>();
-
-    addRoute(router, 'GET', '/user', () => "0");
-    addRoute(router, 'GET', '/user/comments', () => "1");
-    addRoute(router, 'GET', '/user/avatar', () => "2");
-    addRoute(router, 'GET', '/user/lookup/username/:a', (params) => "3: " + params.a);
-    addRoute(router, 'GET', '/user/lookup/email/:a', (params) => "4: " + params.a);
-    addRoute(router, 'GET', '/event/:a', (params) => '5: ' + params.a);
-    addRoute(router, 'GET', '/event/:a/comments', (params) => "6: " + params.a);
-    addRoute(router, 'GET', '/map/:a/events', (params) => '7: ' + params.a);
-    addRoute(router, 'GET', '/status', () => '8');
-    addRoute(router, 'GET', '/very/deeply/nested/route/hello/there', () => '9');
-
-    addRoute(router, 'POST', '/event/:a/comment', (params) => '10: ' + params.a);
-
-    const compiledMatch = compileRouter(router);
+    const compiledMatch = compileRouter(basicAPI());
 
     return (method, path) => {
       const match = compiledMatch(method, path);
@@ -37,30 +40,13 @@ export const jit = {
 
 export const tree = {
   'basic-api': () => {
-    type Handler = (params: Record<string, string>) => string;
-    const router = createRouter<Handler>();
-
-    addRoute(router, 'GET', '/user', () => "0");
-    addRoute(router, 'GET', '/user/comments', () => "1");
-    addRoute(router, 'GET', '/user/avatar', () => "2");
-    addRoute(router, 'GET', '/user/lookup/username/:a', (params) => "3: " + params.a);
-    addRoute(router, 'GET', '/user/lookup/email/:a', (params) => "4: " + params.a);
-    addRoute(router, 'GET', '/event/:a', (params) => '5: ' + params.a);
-    addRoute(router, 'GET', '/event/:a/comments', (params) => "6: " + params.a);
-    addRoute(router, 'GET', '/map/:a/events', (params) => '7: ' + params.a);
-    addRoute(router, 'GET', '/status', () => '8');
-    addRoute(router, 'GET', '/very/deeply/nested/route/hello/there', () => '9');
-
-    addRoute(router, 'POST', '/event/:a/comment', (params) => '10: ' + params.a);
+    const router = basicAPI();
 
     return (method, path) => {
       const match = findRoute(router, method, path);
       return typeof match === 'undefined'
         ? ''
-        : typeof match.params === 'undefined'
-          // @ts-ignore
-          ? match.data()
-          : match.data(match.params)
+        : match.data(match.params)
     }
   }
 } satisfies Subject
