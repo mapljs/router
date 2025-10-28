7e06033e00aac2d0461a934646cbbf8cd465a8a4
aquapi <aquaplmc@gmail.com>
2025-10-29 00:20:52 +0700

    add arg cache util

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,20 @@ export const countParams = (path: string): number => {
   );
   return cnt;
 };
+
+// Cache args
+declare const _: unique symbol;
+export type ArgsCache = string[] & { [_]: '' };
+
+export const paramsCache = (paramCnt: number): ArgsCache => {
+  const arr: ArgsCache = ['', constants.PARAMS + 1] as any;
+  for (let i = 2; i <= paramCnt; i++)
+    arr.push(arr[i - 1] + ',' + constants.PARAMS + i);
+  return arr;
+};
+
+export const addArg = (name: string, cache: ArgsCache): ArgsCache => {
+  const arr: ArgsCache = [cache[0] === '' ? name : cache[0] + name] as any;
+  for (let i = 1; i < cache.length; i++) arr.push(cache[i] + ',' + name);
+  return arr;
+};
