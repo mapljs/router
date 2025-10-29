6de33453ce3f8747ccf17571efb0c069dc7eb8c1
aquapi <aquaplmc@gmail.com>
2025-10-29 14:50:42 +0700

    update

--- src/utils.ts
+++ src/utils.ts
@@ -12,15 +12,17 @@ export const countParams = (path: string): number => {
 declare const _: unique symbol;
 export type ArgsCache = string[] & { [_]: '' };
 
-export const paramsCache = (paramCnt: number): ArgsCache => {
-  const arr: ArgsCache = ['', constants.PARAMS + 1] as any;
+export const paramsCache = (prefix: string, paramCnt: number): ArgsCache => {
+  const arr: ArgsCache = ['', prefix + 1] as any;
+  prefix = ',' + prefix;
   for (let i = 2; i <= paramCnt; i++)
-    arr.push(arr[i - 1] + ',' + constants.PARAMS + i);
+    arr.push(arr[i - 1] + prefix + i);
   return arr;
 };
 
 export const addArg = (name: string, cache: ArgsCache): ArgsCache => {
   const arr: ArgsCache = [cache[0] === '' ? name : cache[0] + ',' + name] as any;
-  for (let i = 1; i < cache.length; i++) arr.push(cache[i] + ',' + name);
+  name = ',' + name;
+  for (let i = 1; i < cache.length; i++) arr.push(cache[i] + name);
   return arr;
 };
