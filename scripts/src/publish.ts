81c9fb71665055137ca46c6321f52778ae4c4912
aquapi <aquaplmc@gmail.com>
2026-03-29 01:16:39 +0700

    update

--- scripts/src/publish.ts
+++ scripts/src/publish.ts
@@ -1,4 +1,4 @@
 import { $ } from 'bun';
 import { LIB } from '../lib/constants.ts';
 
-$`cd ${LIB} && npm publish --access=public --otp=${process.argv.at(2) ?? prompt('OTP:')}`;
+await $`cd ${LIB} && npm publish --access=public --otp=${process.argv.at(2) ?? prompt('OTP:')}`;
